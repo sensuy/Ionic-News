dffc5d41c235c1fef03ffbccae2bcfce4d3bc7e6
sensui <sensuijunior@hotmail.com>
2020-04-23 02:52:06 -0300

    first commit

--- src/app/pages/tab3/tab3.module.ts
+++ src/app/pages/tab3/tab3.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module'
 
@@ -19,4 +19,4 @@ import { Tab3PageRoutingModule } from './tab3-routing.module'
   ],
   declarations: [Tab3Page]
 })
-export class Tab3PageModule {}
+export class Tab3PageModule { }
