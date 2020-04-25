59994f87fbde8ddafc1601d5414723875ae7be9b
sensui <sensuijunior@hotmail.com>
2020-04-25 02:06:21 -0300

    segment category

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonSegmentButton, IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  @ViewChild(IonSegment, { static: true }) segment: IonSegment;
+
+  categorias = ['general', 'business', 'entertainment', 'health', 'science', 'sports', 'technology'];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.segment.value = this.categorias[0];
+  }
 
 }
